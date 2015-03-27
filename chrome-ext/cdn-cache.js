615e5270582fd4b93dc7fb92a25b6fbbf85d022a
Gabriel Brunacci <gbrunacci@gmail.com>
2015-03-26 22:11:06 -0300

    Client-Server approach

--- chrome-ext/cdn-cache.js
+++ chrome-ext/cdn-cache.js
@@ -1,52 +1,19 @@
 var CDNCache = {
 
+    localCDNPath: 'http://localhost:8890/',
+
     domains: [
         'cdnjs.cloudflare.com',
         'cdn.jsdelivr.net',
         'maxcdn.bootstrapcdn.com',
-        'cdn.datatables.net'
+        'cdn.datatables.net',
+        'cdns.gigya.com'
     ],
 
     inDomain: function(path) {
         var a = document.createElement('a');
         a.href = path;
         return CDNCache.domains.indexOf(a.hostname) !== -1;
-    },
-
-    getFromCache: function(path) {
-        return localStorage.getItem(path);
-    },
-
-    store: function(path) {
-        // Too bad, we are doing another request! =(
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", path + "?ignorecdncache", true);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == 4) {
-                // Only cache if successful
-                if ( xhr.status >= 200 && xhr.status < 300 ) {
-                    var contentType = xhr.getResponseHeader('content-type');
-                    //// If a CSS file contains an URL, we can't cache
-                    //// As any redirect will change the reference
-                    //if ( xhr.responseText.indexOf('url(') !== -1 )
-                    //    return;
-                    //
-                    var base64Body = '';
-                    try {
-                        // try doing a simple base64 conversion
-                        base64Body = btoa(xhr.responseText);
-                    } catch (e) {
-                        // try doing a utf8-escaped base64 conversion
-                        base64Body = btoa(unescape(encodeURIComponent(xhr.responseText)));
-                    }
-                    var base64Response = "data:" + contentType + ";base64," + base64Body;
-                    localStorage.setItem(path, base64Response);
-                    delete base64Body;
-                    delete base64Response;  // this one can be quite big
-                }
-            }
-        }
-        xhr.send();
     },
 
     get: function(request) {
@@ -56,17 +23,15 @@ var CDNCache = {
             };
         }
 
-        var cachedElement = CDNCache.getFromCache(request.url);
-        if (cachedElement) {
-            return {
-                redirectUrl: cachedElement
-            };
-        }
+        var a = document.createElement('a');
+        a.href = request.url;
 
-        CDNCache.store(request.url);
+        var newUrl = CDNCache.localCDNPath;//+ '?_ir=cdn-cache&_u=' + encodeURIComponent(request.url  )
+        newUrl += encodeURIComponent(a.protocol + "//" + a.host);
+        newUrl += a.pathname + a.search + a.hash;
 
         return {
-            cancel: false
+            redirectUrl: newUrl
         };
     }
 
---
+++ server/run.py
@@ -0,0 +1,67 @@
+import time
+import BaseHTTPServer
+from urlparse import parse_qs, urljoin
+from urllib import unquote
+import urllib2
+import pprint
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 8890
+
+cache = {}
+
+class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_HEAD(self):
+        """Respond to a HEAD request."""
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+    def do_GET(self):
+        """Respond to a GET request."""
+        opener = urllib2.build_opener()
+        opener.addheaders = [('Accept', '*/*'), ('Host', 'localhost')]
+
+        path_pieces = self.path.split('/')
+        base_domain = unquote(path_pieces[1])
+        path = "/".join(path_pieces[2:])
+
+        if base_domain and path:
+            url = base_domain + "/" + path
+            if url in cache:
+                response = cache[url]
+                response_code = response['code']
+                response_info = response['info']
+                response_body = response['body']
+            else:
+                response = opener.open(url)
+                response_info = response.info()
+                response_body = response.read()
+                response_code = response.getcode()
+                cache[url] = {
+                    "code": response_code,
+                    "info": response_info,
+                    "body": response_body
+                }
+
+            self.send_response(response_code)
+            for header in response_info.headers:
+                header_parts = header.split(':')
+                self.send_header(header_parts[0], ":".join(header_parts[1:])[:-2])
+            self.end_headers()
+            self.wfile.write(response_body)
+            self.wfile.close()
+            return
+
+        self.send_response(404)
+        self.end_headers()
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
