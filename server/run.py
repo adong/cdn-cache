3d95ec190d83eda50dd5eea3363b779f05779337
Gabriel Brunacci <gbrunacci@gmail.com>
2015-06-05 20:39:07 -0300

    Rewriten server in Bottle

--- server/run.py
+++ server/run.py
@@ -1,79 +1,52 @@
-import time
 from urlparse import parse_qs, urljoin
 from urllib import unquote
 import urllib2
-from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-from SocketServer import ThreadingMixIn
-import threading
-
-HOST_NAME = 'localhost'
-PORT_NUMBER = 8890
+from bottle import route, run, response, request, abort, debug
 
 cache = {}
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_HEAD(self):
-        """Respond to a HEAD request."""
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-    def do_GET(self):
-        """Respond to a GET request."""
-        opener = urllib2.build_opener()
-        opener.addheaders = [('Accept', '*/*'), ('Host', 'localhost')]
+@route('/<path:re:.*>')
+def index(path):
+    if path == 'is-alive':
+        return ""
 
-        if self.path == "/is-alive":
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.close()
-            return
+    if request.method == 'HEAD':
+        return ""
 
-        path_pieces = self.path.split('/')
-        base_domain = unquote(path_pieces[1])
-        path = "/".join(path_pieces[2:])
+    opener = urllib2.build_opener()
+    opener.addheaders = [('Accept', '*/*'), ('Host', 'localhost')]
 
-        if base_domain and path:
-            url = base_domain + "/" + path
-            if url in cache:
-                response = cache[url]
-                response_code = response['code']
-                response_info = response['info']
-                response_body = response['body']
-            else:
-                response = opener.open(url)
-                response_info = response.info()
-                response_body = response.read()
-                response_code = response.getcode()
-                cache[url] = {
-                    "code": response_code,
-                    "info": response_info,
-                    "body": response_body
-                }
+    path_pieces = path.split('/')
+    base_domain = unquote(path_pieces[0])
+    path = "/".join(path_pieces[1:])
 
-            self.send_response(response_code)
-            for header in response_info.headers:
-                header_parts = header.split(':')
-                self.send_header(header_parts[0], ":".join(header_parts[1:])[:-2])
-            self.end_headers()
-            self.wfile.write(response_body)
-            self.wfile.close()
-            return
+    if base_domain and path:
+        url = base_domain + "/" + path
+        if url in cache:
+            url_response = cache[url]
+            response_code = url_response['code']
+            response_info = url_response['info']
+            response_body = url_response['body']
+        else:
+            url_response = opener.open(url)
+            response_info = url_response.info()
+            response_body = url_response.read()
+            response_code = url_response.getcode()
+            cache[url] = {
+                "code": response_code,
+                "info": response_info,
+                "body": response_body
+            }
 
-        self.send_response(404)
-        self.end_headers()
+        response.status = response_code
+        for header in response_info.headers:
+            header_parts = header.split(':')
+            if header_parts[0].lower().strip() in ['connection', 'keep-alive', 'proxy-authenticate', 'proxy-authorization', 'te', 'trailers', 'transfer-encoding', 'upgrade']:
+                continue
+            response.set_header(header_parts[0], ":".join(header_parts[1:])[:-2])
 
+        return response_body
 
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    """Handle requests in a separate thread."""
+    abort(404)
 
-
-if __name__ == '__main__':
-    httpd = ThreadedHTTPServer((HOST_NAME, PORT_NUMBER), MyHandler)
-    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
-
+run(host='localhost', port=8890, server='cherrypy')
